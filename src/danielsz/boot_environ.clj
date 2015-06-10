091201426338904eccaf2001c547e7c6657dbb92
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-06-10 14:48:45 +0300

    version 0.0.4

--- src/danielsz/boot_environ.clj
+++ src/danielsz/boot_environ.clj
@@ -4,10 +4,11 @@
    [clojure.java.io :as io]
    [boot.core       :as core]
    [boot.util       :as util]
-   [environ.core :refer [env]]))
+   [environ.core :as environ]))
 
 (core/deftask environ [e env FOO=BAR {kw edn} "The environment map"]
-  (core/with-pre-wrap fileset
-    (boot.util/info (str "environment " env "\n"))
-    (alter-var-root #'env merge env)
-    fileset))
+  (fn [next-task]
+    (fn [fileset]
+      (boot.util/info (str "environment " env "\n"))
+      (with-redefs [environ/env (merge environ/env env)]
+        (next-task fileset)))))
